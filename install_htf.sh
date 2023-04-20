88dec24b1119b33d6d8dcf602021f0fa23680a39
khatharsis <hadrien.guelque@telecom-sudparis.eu>
2023-04-20 11:20:28 +0200

    Removed private scripts.

--- install_htf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd $PILGRIM_PATH/src/Hierarchical-Trace-Format
-rm -rf build && mkdir build && cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$PILGRIM_PATH
-make && make install
-cd $PILGRIM_PATH/src/eztrace
-rm -rf build && mkdir build && cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$PILGRIM_PATH
-make && make install
