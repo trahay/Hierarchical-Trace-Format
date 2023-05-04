2b2dd95c2f162e4488b2af24d124cfd2d32468a7
khatharsis <hadrien.guelque@telecom-sudparis.eu>
2023-05-04 12:47:39 +0200

    Some documentation.
    Signed-off-by: khatharsis <hadrien.guelque@telecom-sudparis.eu>

--- src/htf_read.h
+++ src/htf_read.h
@@ -4,24 +4,33 @@
 #include "htf.h"
 
 struct htf_thread_reader {
+	/** Archive being read by this reader. */
 	struct htf_archive* archive;
+	/** Thread being read. */
 	struct htf_thread* thread_trace;
 
-	htf_token_t* callstack_sequence; /* each entry contains the sequence/loop being read */
-	int* callstack_index;						 /* each entry contains the index in the sequence or the loop iteration */
-	int*
-			callstack_loop_iteration; /* each entry contains the number of iteration of the loop at the corresponding frame */
+	/** Stack containing the sequences/loops being read. */
+	htf_token_t* callstack_sequence;
 
+	/** Stack containing the index in the sequence or the loop iteration. */
+	int* callstack_index;
+
+	/** Stack containing the number of iteration of the loop at the corresponding frame */
+	int* callstack_loop_iteration;
+
+	/** Current frame = index of the event/loop being read in the callstacks.
+	 * You can view this as the "depth" of the callstack. */
 	int current_frame;
 
+	/** At any point, an event e has been seen event_index[e.id] times.
+	 * Use this to grab the timestamps and other information on the event. */
 	int* event_index;
 
 	/**
-	 * Current depth, ie number of sequences + loop we are in. Only used in cosmetics.
+	 * Current depth, ie number of sequences + loop we are in. Only used in cosmetics, to print the structure.
 	 */
 	int depth;
 };
-
 
 void htf_read_global_archive(struct htf_archive* archive, char* main_filename);
 
@@ -31,28 +40,30 @@ void htf_read_thread_iterator_init(struct htf_archive *archive,
 				   struct htf_thread_reader *reader,
 				   htf_thread_id_t thread_id);
 
-/* return the current event in a thread and move to the next one.
- * Return -1 in case of an error (such as the end of the trace)
+/** Copies the value of the current event in a thread to the given pointer, and moves to the next one.
+ * @return -1 in case of an error (such as the end of the trace), else 0.
  */
 int htf_read_thread_next_event(struct htf_thread_reader* reader, struct htf_event_occurence* e);
-/* return the current event in a thread.
- * Return -1 in case of an error (such as the end of the trace)
+/** Copies the value of the current event in a thread to the given pointer.
+ * @return -1 in case of an error (such as the end of the trace), else 0.
  */
 int htf_read_thread_cur_event(struct htf_thread_reader* reader, struct htf_event_occurence* e);
 /**
  * Copies the value of the current token in a thread to the given pointer.
  * If it is an event, copies the informations to the event_occurence pointer.
+ * Be aware that exiting a block is not a token, but entering is.
  * @return -1 in case of an error (such as the end of the trace), else 0.
  */
 int htf_read_thread_cur_token(struct htf_thread_reader* reader, struct htf_token* t, struct htf_event_occurence* e);
 /**
- * Copies the value of the current token in a thread to the given pointer, and advances in the trace.
+ * Copies the value of the current token in a thread to the given pointer, and moves to the next one.
  * If it is an event, copies the informations to the event_occurence pointer.
+ * Be aware that exiting a block is not a token, but entering is.
  * @return -1 in case of an error (such as the end of the trace), else 0.
  */
 int htf_read_thread_next_token(struct htf_thread_reader* reader, struct htf_token* t, struct htf_event_occurence* e);
 /**
- * Grabs the timestamps at which the given Sequence or Loop starts.
+ * @return The timestamps at which the given Sequence or Loop starts, without modifying the reader's state.
  */
 htf_timestamp_t htf_get_starting_timestamp(struct htf_thread_reader* reader, struct htf_token token);
 #define MAX_CALLSTACK_DEPTH 100
