063e39bf7990474f135d5ede1cc5fabb00f2f4bd
François Trahay <francois.trahay@telecom-sudparis.eu>
2023-07-12 13:32:16 +0200

    formatting

--- tracer/tracer.c
+++ tracer/tracer.c
@@ -1,11 +1,15 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+/*
+ * Copyright (C) Telecom SudParis
+ * See LICENSE in top-level directory.
+ */
 #include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "htf.h"
-#include "htf_write.h"
 #include "htf_archive.h"
+#include "htf_write.h"
 #include "tracer.h"
 
 static struct htf_archive trace;
@@ -17,7 +21,7 @@ static _Atomic int nb_threads = 0;
 static struct htf_thread_writer** thread_writers = NULL;
 static pthread_mutex_t lock;
 
-static int verbose=0;
+static int verbose = 0;
 static int initialized = 0;
 
 static htf_string_ref_t _register_string(char* str) {
@@ -41,12 +45,13 @@ static htf_thread_id_t _new_thread() {
 }
 
 void _init_thread() {
-  if(! initialized) return;
+  if (!initialized)
+    return;
 
   pthread_mutex_lock(&lock);
   thread_rank = nb_threads++;
   thread_writer = malloc(sizeof(struct htf_thread_writer));
-  thread_writers = realloc(thread_writers, sizeof(struct htf_thread_writer*)* nb_threads);
+  thread_writers = realloc(thread_writers, sizeof(struct htf_thread_writer*) * nb_threads);
   thread_writers[thread_rank] = thread_writer;
   pthread_mutex_unlock(&lock);
 
@@ -55,37 +60,35 @@ void _init_thread() {
   htf_string_ref_t thread_name_id = _register_string(thread_name);
 
   thread_id = _new_thread();
-  htf_write_define_location(&trace,
-			    thread_id,
-			    thread_name_id,
-			    process_id);
+  htf_write_define_location(&trace, thread_id, thread_name_id, process_id);
 
   htf_write_thread_open(&trace, thread_writer, thread_id);
 }
 
 void enter_function(enum intercepted_function f, void* ptr) {
-  if(! initialized) return;
+  if (!initialized)
+    return;
 
-  if(thread_writer == NULL)
+  if (thread_writer == NULL)
     _init_thread();
 
   htf_record_enter(thread_writer, NULL, HTF_TIMESTAMP_INVALID, (int)f);
 }
 
 void leave_function(enum intercepted_function f, void* ptr) {
-  if(! initialized) return;
+  if (!initialized)
+    return;
 
   htf_record_leave(thread_writer, NULL, HTF_TIMESTAMP_INVALID, (int)f);
 }
 
-void* get_callback(const char*fname){
+void* get_callback(const char* fname) {
   void* ret = dlsym(RTLD_NEXT, fname);
-  if(!ret) {
+  if (!ret) {
     fprintf(stderr, "Warning: cannot find %s: %s\n", fname, dlerror());
   }
   return ret;
 }
-
 
 static void _get_trace_name(char* buffer) {
   char tmp_buffer[1024];
@@ -100,12 +103,12 @@ static void _tracer_init(void) {
   printf("[TRACER] initializing\n");
 
   char* verbose_str = getenv("VERBOSE");
-  if(verbose_str)
+  if (verbose_str)
     verbose = 1;
 
   char* base_dirname = getenv("TRACE_FILENAME");
-  if(! base_dirname) {
-    base_dirname=malloc(sizeof(char)*100);
+  if (!base_dirname) {
+    base_dirname = malloc(sizeof(char) * 100);
     snprintf(base_dirname, 100, "trace");
   }
 
@@ -113,19 +116,13 @@ static void _tracer_init(void) {
 
   char program_name[1024];
   _get_trace_name(program_name);
-  
-  htf_write_archive_open(&trace,
-			 program_name,
-			 "main",
-			 0);
+
+  htf_write_archive_open(&trace, program_name, "main", 0);
 
   process_id = _new_location_group();
-  htf_write_define_location_group(&trace,
-				  process_id,
-				  _register_string("Process"),
-				  HTF_LOCATION_GROUP_ID_INVALID);
-  
-  for(int i = 0; i<NB_FUNCTIONS; i++) {
+  htf_write_define_location_group(&trace, process_id, _register_string("Process"), HTF_LOCATION_GROUP_ID_INVALID);
+
+  for (int i = 0; i < NB_FUNCTIONS; i++) {
     htf_string_ref_t string_ref = _register_string(function_names[i]);
     htf_archive_register_region(&trace, i, string_ref);
   }
@@ -137,9 +134,17 @@ static void _tracer_conclude(void) __attribute__((destructor));
 static void _tracer_conclude(void) {
   printf("[TRACER] finalizing write_events\n");
 
-  for(int i=0; i<nb_threads; i++) {
+  for (int i = 0; i < nb_threads; i++) {
     htf_write_thread_close(thread_writers[i]);
   }
 
   htf_write_archive_close(&trace);
 }
+
+/* -*-
+   mode: c;
+   c-file-style: "k&r";
+   c-basic-offset 2;
+   tab-width 2 ;
+   indent-tabs-mode nil
+   -*- */
--- tracer/tracer.h
+++ tracer/tracer.h
@@ -1,55 +1,53 @@
+/*
+ * Copyright (C) Telecom SudParis
+ * See LICENSE in top-level directory.
+ */
 #ifndef TRACER_H
 #define TRACER_H
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <dlfcn.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "htf.h"
 
 #ifdef DEBUG
 #define DEBUG_PRINTF(...) printf(__VA_ARGS__)
 #else
-#define DEBUG_PRINTF(...) (void) 0
+#define DEBUG_PRINTF(...) (void)0
 #endif
 
+void* get_callback(const char* fname);
 
-void* get_callback(const char *fname);
- 
+static char* function_names[] __attribute__((unused)) = {
+  "mutex_lock",     "mutex_trylock", "mutex_unlock",   "mutex_init", "mutex_destroy", "cond_wait",
+  "cond_timedwait", "cond_signal",   "cond_broadcast", "cond_init",  "cond_destroy"};
 
-static char* function_names [] __attribute__((unused)) = {
-   "mutex_lock",
-   "mutex_trylock",
-   "mutex_unlock",
-   "mutex_init",
-   "mutex_destroy",
-   "cond_wait",
-   "cond_timedwait",
-   "cond_signal",
-   "cond_broadcast",
-   "cond_init",
-   "cond_destroy"
+enum intercepted_function {
+  mutex_lock,
+  mutex_trylock,
+  mutex_unlock,
+  mutex_init,
+  mutex_destroy,
+  cond_wait,
+  cond_timedwait,
+  cond_signal,
+  cond_broadcast,
+  cond_init,
+  cond_destroy,
+  NB_FUNCTIONS,
 };
-
-
-enum intercepted_function
-  {
-   mutex_lock,
-   mutex_trylock,
-   mutex_unlock,
-   mutex_init,
-   mutex_destroy,
-   cond_wait,
-   cond_timedwait,
-   cond_signal,
-   cond_broadcast,
-   cond_init,
-   cond_destroy,
-   NB_FUNCTIONS,
-  };
 
 void enter_function(enum intercepted_function f, void* ptr);
 void leave_function(enum intercepted_function f, void* ptr);
 
-#endif	/* TRACER_H */
+#endif /* TRACER_H */
+
+/* -*-
+   mode: c;
+   c-file-style: "k&r";
+   c-basic-offset 2;
+   tab-width 2 ;
+   indent-tabs-mode nil
+   -*- */
