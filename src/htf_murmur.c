5ccd7d2724cf0b9a7083412b8db1672162a2faa8
François Trahay <francois.trahay@telecom-sudparis.eu>
2023-07-13 14:47:39 +0200

    hash 32 bits

--- src/htf_murmur.c
+++ src/htf_murmur.c
@@ -20,10 +20,15 @@
 #define FORCE_INLINE inline
 #endif
 
+static FORCE_INLINE uint32_t rotl32 ( uint32_t x, int8_t r ) {
+  return (x << r) | (x >> (32 - r));
+}
+
 static FORCE_INLINE uint64_t rotl64(uint64_t x, int8_t r) {
   return (x << r) | (x >> (64 - r));
 }
 
+#define ROTL32(x, y) rotl32(x, y)
 #define ROTL64(x, y) rotl64(x, y)
 
 #define BIG_CONSTANT(x) (x##LLU)
@@ -37,6 +42,16 @@ static FORCE_INLINE uint64_t rotl64(uint64_t x, int8_t r) {
 //-----------------------------------------------------------------------------
 // Finalization mix - force all bits of a hash block to avalanche
 
+static FORCE_INLINE uint32_t fmix32 ( uint32_t h ) {
+  h ^= h >> 16;
+  h *= 0x85ebca6b;
+  h ^= h >> 13;
+  h *= 0xc2b2ae35;
+  h ^= h >> 16;
+
+  return h;
+}
+
 static FORCE_INLINE uint64_t fmix64(uint64_t k) {
   k ^= k >> 33;
   k *= BIG_CONSTANT(0xff51afd7ed558ccd);
@@ -48,10 +63,60 @@ static FORCE_INLINE uint64_t fmix64(uint64_t k) {
 }
 
 //-----------------------------------------------------------------------------
-void htf_murmur_hash3_x64_32(const void* key, const int len, const uint32_t seed, uint32_t* out) {
-  uint32_t buffer[2];
-  htf_murmur_hash3_x64_64(key, len, seed, (uint64_t*)buffer);
-  *out = buffer[0];
+
+void htf_murmur_hash3_x64_32 ( const void * key, int len, uint32_t seed, uint32_t * out )
+{
+  const uint8_t * data = (const uint8_t*)key;
+  const int nblocks = len / 4;
+
+  uint32_t h1 = seed;
+
+  const uint32_t c1 = 0xcc9e2d51;
+  const uint32_t c2 = 0x1b873593;
+
+  //----------
+  // body
+
+  const uint32_t * blocks = (const uint32_t *)(data + nblocks*4);
+
+  for(int i = -nblocks; i; i++)
+  {
+    uint32_t k1 = getblock(blocks,i);
+
+    k1 *= c1;
+    k1 = ROTL32(k1,15);
+    k1 *= c2;
+    
+    h1 ^= k1;
+    h1 = ROTL32(h1,13); 
+    h1 = h1*5+0xe6546b64;
+  }
+
+  //----------
+  // tail
+
+  const uint8_t * tail = (const uint8_t*)(data + nblocks*4);
+
+  uint32_t k1 = 0;
+
+  switch(len & 3)
+  {
+  case 3: k1 ^= tail[2] << 16;
+    [[fallthrough]];
+  case 2: k1 ^= tail[1] << 8;
+    [[fallthrough]];
+  case 1: k1 ^= tail[0];
+          k1 *= c1; k1 = ROTL32(k1,15); k1 *= c2; h1 ^= k1;
+  };
+
+  //----------
+  // finalization
+
+  h1 ^= len;
+
+  h1 = fmix32(h1);
+
+  *(uint32_t*)out = h1;
 }
 
 void htf_murmur_hash3_x64_64(const void* key, const int len, const uint32_t seed, uint64_t* out) {
