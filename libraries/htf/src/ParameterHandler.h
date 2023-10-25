7af065e967e5293bc2f00a524adb76c5ad72a17f
Catherine Guelque <guelque@telecom-sudparis.eu>
2023-10-25 13:47:03 +0200

    Doxygen-approved documentation for ParameterHandler.h
    Signed-off-by: Catherine Guelque <guelque@telecom-sudparis.eu>

--- libraries/htf/src/ParameterHandler.h
+++ libraries/htf/src/ParameterHandler.h
@@ -45,22 +45,51 @@ enum LoopFindingAlgorithm {
   LoopFindingBasicTruncated
 };
 
+/**
+ * @brief A simple data class that contains information on different parameters.
+ */
 class ParameterHandler {
+  /** The compression algorithm used during the execution. */
   CompressionAlgorithm compressionAlgorithm{CompressionNone};
+  /** The ZSTD compression level. */
   size_t zstdCompressionLevel{1};
+  /** The encoding algorithm used during the execution. */
   EncodingAlgorithm encodingAlgorithm{EncodingNone};
+  /** The compression algorithm used during the execution. */
   LoopFindingAlgorithm loopFindingAlgorithm{LoopFindingBasicTruncated};
+  /** The max length the ::LoopFindingBasicTruncated will go to.*/
   size_t maxLoopLength{100};
 
  public:
+  /** Getter for maxLoopLength. Error if you're not supposed to have a maxLoopLength.
+   * @returns Value of maxLoopLength. */
   [[nodiscard]] size_t getMaxLoopLength() const;
+  /** Getter for zstdCompressionLevel. Error if you're not using ZSTD.
+   * @returns Value of zstdCompressionLevel. */
   [[nodiscard]] u_int8_t getZstdCompressionLevel() const;
+  /** Getter for compressionAlgorithm.
+   * @returns Value of compressionAlgorithm. */
   [[nodiscard]] CompressionAlgorithm getCompressionAlgorithm() const;
+  /**
+   * Getter for encodingAlgorithm.
+   * @returns Value of encodingAlgorithm. If the compressionAlgorithm is lossy, returns EncodingNone. */
   [[nodiscard]] EncodingAlgorithm getEncodingAlgorithm() const;
+  /**
+   * Getter for loopFindingAlgorithm.
+   * @returns Value of loopFindingAlgorithm.
+   */
   [[nodiscard]] LoopFindingAlgorithm getLoopFindingAlgorithm() const;
+  /**
+   * Creates a parameterHandler from a config file.
+   * @param configFileName Path to a valid JSON config file.
+   */
   explicit ParameterHandler(const std::string& configFileName);
+  /**
+   * Default constructor.
+   */
   ParameterHandler() = default;
 };
 
+/** Global ParameterHandler. This is supposed to be the only instance of that class. */
 extern const ParameterHandler parameterHandler;
 }  // namespace htf
